8cb0b259aadd8f402ca5eb9d6d3cf21093136270
Sonia Klimas <sonia.klimas@gmail.com>
2020-04-22 10:23:59 +0200

    Updates

---
+++ precache-manifest.2edca9b34f222065fbc6c252657f7593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc3ba1455fb5eda228494ecd6e1febd",
+    "url": "/FavNote_basic/index.html"
+  },
+  {
+    "revision": "4fff137f53e6f7a2647c",
+    "url": "/FavNote_basic/static/css/main.d1296072.chunk.css"
+  },
+  {
+    "revision": "af6d3a5bc3c7ee831eac",
+    "url": "/FavNote_basic/static/js/2.de2c5d93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FavNote_basic/static/js/2.de2c5d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fff137f53e6f7a2647c",
+    "url": "/FavNote_basic/static/js/main.2f4a6e5c.chunk.js"
+  },
+  {
+    "revision": "e33cb95884f35ccdba7e",
+    "url": "/FavNote_basic/static/js/runtime-main.7d89bcde.js"
+  },
+  {
+    "revision": "c00fcedae382b8e10e065c7cc5c0742b",
+    "url": "/FavNote_basic/static/media/logo.c00fceda.png"
+  }
+]);
